8d0e4e900cd141b8f9d3121cba1e95f688a83bd5
Cullen Pu <cullenpu@gmail.com>
2021-01-15 20:29:38 +0000

    Implement forward and backprogation functions

--- R/nn.R
+++ R/nn.R
@@ -8,7 +8,7 @@
 #' @param num_hidden_2 dimension of second hidden layer
 #' @param num_outputs dimension of output
 #'
-#' @return dictionary containing weight and biase matrices in each layer of the network
+#' @return list containing weight and bias matrices in each layer of the network
 #' @export
 #'
 #' @examples
@@ -19,7 +19,7 @@ init_nn <- function(num_inputs, num_hidden_1, num_hidden_2, num_outputs) {
   b1 <- rep(0, num_hidden_1)
   b2 <- rep(0, num_hidden_2)
   b3 <- rep(0, num_outputs)
-  model <- dict(list('w1' = w1, 'w2' = w2, 'w3' = w3, 'b1' = b1, 'b2' = b2, 'b3' = b3))
+  model <- list("w1" = w1, "w2" = w2, "w3" = w3, "b1" = b1, "b2" = b2, "b3" = b3)
   return(model)
 }
 
@@ -33,7 +33,7 @@ affine_back <- function(grad_y, x, w) {
   grad_x <- grad_y %*% t(w)
   grad_w <- t(x) %*% grad_y
   grad_b <- grad_y
-  return(list(grad_x, grad_w, grad_b))
+  return(list("grad_x" = grad_x, "grad_w" = grad_w, "grad_b" = grad_b))
 }
 
 relu <- function(x) {
@@ -44,4 +44,26 @@ relu_back <- function(grad_y, x) {
   grad_x <- grad_y
   grad_x[x <= 0] <- 0
   return(grad_x)
+}
+
+softmax <- function(x) {
+  return(exp(x) / sum(x))
+}
+
+forwardprop <- function(model, x) {
+  z1 <- affine(x, model["w1"], model$get["b1"])
+  h1 <- relu(z1)
+  z2 <- affine(h1, model["w2"], model$get["b2"])
+  h2 <- relu(z2)
+  y <- affine(h2, model["w3"], model$get["b3"])
+  forward_pass <- list("x" = x, "z1" = z1, "h1" = h1, "z2" = z2, "h2" = h2, "y" = y)
+  return(forward_pass)
+}
+
+backprop <- function(model, error, forward_pass) {
+  affine1 <- affine_back(error, forward_pass["h2"], model["w3"])
+  relu1 <- relu_back(affine1["grad_x"], forward_pass["z2"])
+  affine2 <- affine_back(relu1, forward_pass["h1"], model["w2"])
+  relu2 <- relu_back(affine2["grad_x"], forward_pass["z1"])
+  affine3 <- affine_back(relu2, forward_pass["x"], model["w1"])
 }
